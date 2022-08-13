57800b8bbeab75e7a039ed870c3b421efb72e485
RickGrimesProgramador <109391965+RickGrimesProgramador@users.noreply.github.com>
2022-08-12 22:34:50 -0300

    Add files via upload

---
+++ source/repos/Client/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+                socket.Connect(endPoint);
+
+                Console.WriteLine("Conectado!");
+                while (0 != 1)
+                {
+                    Console.WriteLine("Escreva algo: ");
+                    String info = Console.ReadLine();
+                    Console.WriteLine();
+                    byte[] infoEnviar = Encoding.Default.GetBytes(info);
+
+                    socket.Send(infoEnviar, infoEnviar.Length, SocketFlags.None);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não foi possivel conectar ao servidor !");
+            }
+        }
+    }
+
+}
---
+++ source/repos/server/server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+            socket.Bind(endpoint);
+
+            socket.Listen(5);
+
+            Console.WriteLine("Server aberto ");
+            Console.WriteLine("Qual o nome do seu amigo?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("espere....");
+            Socket escutar = socket.Accept();
+            while (1 != 0)
+            {
+         
+                byte[] bytes = new byte[255];
+
+                int tamanho = escutar.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+
+                Array.Resize(ref bytes, tamanho);
+                Console.Write(" mensagem recebida:  ", nome, " falou : ");
+                Console.Write(Encoding.Default.GetString(bytes));
+                Console.ReadLine();
+
+            }
+
+
+
+        }
+    }
+}
